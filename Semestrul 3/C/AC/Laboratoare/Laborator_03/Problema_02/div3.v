75970014f71a17d52f84bc9ec6f9e7f311870a7e
Daria-1303 <daria.ciobanu@student.upt.ro>
2024-10-14 09:01:54 +0300

    done lab 3

--- Semestrul 3/C/AC/Laboratoare/Laborator_03/Problema_02/div3.v
+++ Semestrul 3/C/AC/Laboratoare/Laborator_03/Problema_02/div3.v
@@ -1,23 +1,38 @@
 module div3 (
   input [3:0] i,
-  output [2:0] o 
+  output reg [2:0] o 
 );
 
 reg [3:0] temp;   
 reg [2:0] count;  
 
+/*
 always @(*) begin
     temp = i;     
     count = 0;     
 
-    if (temp >= 3) begin
+    while (temp >= 3) begin
         temp = temp - 3;      
         count = count + 1;    
     end
     
           
 end
-	assign o = count; 
+*/
+
+always @(*) begin
+	case(i)
+		4'd0, 4'd1, 4'd2 : o = 3'd0;
+		4'd3, 4'd4, 4'd5 : o = 3'd1;
+		4'd6, 4'd7, 4'd8 : o = 3'd2;
+		4'd9, 4'd10, 4'd11 : o = 3'd3;
+		4'd12, 4'd13, 4'd14 : o = 3'd4;
+		4'd15 : o = 3'd5;
+		default : o = 3'd0;
+	endcase
+
+end
+	//assign o = count; 
 endmodule
 
 
